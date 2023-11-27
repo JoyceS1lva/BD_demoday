24f7d614953abbb1391591070f58d9d4a7a5e97f
Joyce Camille <joyce.ssilva02@gmail.com>
2023-11-27 18:18:09 -0300

    inserindo dados

--- bd_vinculos.sql
+++ bd_vinculos.sql
@@ -12,8 +12,16 @@ CREATE TABLE cadastro_empresa (
      senha_empresa_hash VARCHAR(30)
 );
 
+INSERT INTO cadastro_empresa
+(Pk_cnpj,nome_colaborador,email_colaborador,nome_empresa,senha_empresa_hash) Values 
+
+('12.345.678/0001-90', 'Ana Silva', 'contato@tech.com', 'Tech Solutions Ltda','SenhaSegura123'),
+('12.345.768/0001-00', 'Lucas Oliveira', 'contato@inovatech.com', 'InovaTech Solutions','Senha@123'),
+('12.345.678/0001-99', 'Rafael Lima', 'contato@futurotech.com', 'FuturoTech','Segura1326'),
+('98.765.432/0001-21', 'Carolina Santos', 'contato@visionsoft.com', 'VisionSoft Solutions','Senha2013');
+
 -- Tabela cadastro de vaga 
-CREATE TABLE cadastroVaga (
+CREATE TABLE cadastro_vaga (
 	Pk_Idvaga int primary key not null auto_increment,
     nome_vaga VARCHAR(150) NOT NULL,
 	area_atuacao VARCHAR(150),
@@ -22,7 +30,7 @@ CREATE TABLE cadastroVaga (
     localizacao VARCHAR(255),
     descricao TEXT NOT NULL,
     requisitos TEXT NOT NULL,
-    valor_salario decimal(4.2)
+    valor_salario decimal(10.2)
 );
 
 -- Tabela cadastro cursos
@@ -33,12 +41,30 @@ CREATE TABLE cursos (
     comentario TEXT
 );
 
+INSERT INTO cursos
+(avaliacao,nome_curso,comentario) VALUES
+
+('3', 'Desenvolvimento de Habilidades de Comunicação', 'Conteúdo útil, mas as atividades práticas poderiam ser mais desafiadoras.'),
+('4', 'Gestão de Projetos Empresariais', 'Curso essencial para quem busca liderar equipes e projetos com eficiência.'),
+('5', 'Introdução à Estatística', 'Achei o curso muito util e pratico');
+
+
 -- Tabela Cadastro candidato
 CREATE TABLE cadastro_candidato (
 	cpf VARCHAR(14) primary key not null,
     email_candidato VARCHAR(255) NOT NULL,
     senha_candidato_hash VARCHAR(30)
 );
+
+INSERT INTO cadastro_candidato 
+(cpf,email_candidato,senha_candidato_hash) VALUES 
+
+('789.012.345-67', 'rafael@email.com', 'hashjkl'),
+('890.123.456-78', 'carla@email.com', 'hashmno'),
+('901.234.567-89', 'joao@email.com', 'hashpqr'),
+('456.789.012-34', 'diana@email.com', 'hashabc'),
+('567.890.123-45', 'eduardo@email.com', 'hashdef');
+
 
 -- Tabela perfil_candidato
 CREATE TABLE perfil_cand (
@@ -58,3 +84,13 @@ CREATE TABLE perfil_cand (
     email_candidato VARCHAR(255),
     FOREIGN KEY (email_candidato) REFERENCES cadastro_candidato (email_candidato)
 );
+
+INSERT INTO perfil_candidato 
+(nome_completo,nome_social,celular,cep,numero.complemento,data_nasc,nacionalidade,genero,estado_civil)VALUES
+
+('Rafael Silva', 'Rafa', '987654321', '12345-678', 123, 'Apto 456', '1990-05-15', 'Brasileiro', 'Masculino', 'Solteiro'),
+('Carla Oliveira', 'Carlinha', '987123456', '54321-876', 456, 'Casa 789', '1985-12-20', 'Brasileira', 'Feminino', 'Casada'),
+('João Santos', 'Joãozinho', '999888777', '87654-321', 789, 'Bloco C', '1988-08-10', 'Brasileiro', 'Masculino', 'Solteiro'),
+('Diana Pereira', 'Diana', '999111222', '98765-432', 234, 'Andar 5', '1995-03-25', 'Brasileira', 'Feminino', 'Solteira'),
+('Eduardo Souza', 'Dudu', '987654123', '43210-876', 567, 'Cobertura 101', '1982-11-05', 'Brasileiro', 'Masculino', 'Divorciado');
+
